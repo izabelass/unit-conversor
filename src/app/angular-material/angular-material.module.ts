cede3940d8d0dbfaa3d09aaa53768e73ae0bb9f1
GiselleBarbosa <g88barbosa@gmail.com>
2022-12-11 12:44:45 -0300

    add header

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -4,9 +4,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 const materialModules = [
   MatInputModule,
@@ -14,8 +14,9 @@ const materialModules = [
   MatCardModule,
   MatButtonModule,
   MatSelectModule,
-  MatToolbarModule,
+  MatToolbarModule, 
   MatIconModule
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ValueInputComponent } from './value-input/value-input.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConversorComponent,
-    UnitSelectionComponent,
-    ValueInputComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AngularMaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ConversorComponent,
+        UnitSelectionComponent,
+        ValueInputComponent,
+        HeaderComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NgbModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AngularMaterialModule,
+    ]
 })
 export class AppModule { }
